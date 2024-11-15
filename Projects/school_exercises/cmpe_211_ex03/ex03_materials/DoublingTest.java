7cd87075cc4f5a30f04738dbcfe8afe6a07c3246
UmutSogukpinar <usogukpinar0@gmail.com>
2024-11-15 22:01:07 +0300

    update

---
+++ Projects/school_exercises/cmpe_211_ex03/ex03_materials/DoublingTest.java
@@ -0,0 +1,39 @@
+package cmpe_211_ex03.ex03_materials;
+
+public class DoublingTest {
+    private static final int MAXIMUM_INTEGER = 1000000;
+
+    // This class should not be instantiated.
+    private DoublingTest() { }
+
+    /**
+     * Returns the amount of time to call {@code ThreeSum.count()} with <em>n</em>
+     * random 6-digit integers.
+     * @param n the number of integers
+     * @return amount of time (in seconds) to call {@code ThreeSum.count()}
+     *   with <em>n</em> random 6-digit integers
+     */
+
+    public static double timeTrial(int n) {
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = StdRandom.uniformInt(-MAXIMUM_INTEGER, MAXIMUM_INTEGER);
+        }
+        Stopwatch timer = new Stopwatch();
+        int ignore = ThreeSum.count(a);
+        return timer.elapsedTime();
+    }
+
+    /**
+     * Prints table of running times to call {@code ThreeSum.count()}
+     * for arrays of size 250, 500, 1000, 2000, and so forth.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        for (int n = 100; true; n += n) {
+            double time = timeTrial(n);
+            StdOut.printf("%7d %7.1f\n", n, time);
+        }
+    }
+}
